ae12c70bc514e4e7a00368eef49b82dc15652315
Emiliano <noggaxiii@gmail.com>
2017-06-21 12:39:11 -0300

    script para generar grafos funciona correctamente. Seguir instrucciones en script.cpp. Sin embargo, el generador de grafos de grego esta fallando.

--- TestingEmiNipo/script.cpp
+++ TestingEmiNipo/script.cpp
@@ -1,53 +1,83 @@
 #include <iostream>
-
+#include "GeneradorDeGrafosModif.cpp"
 using namespace std;
 
+/*
+
+NOTA IMPORTANTE:
+Para correr los tests y ponerlos en un txt, compilar, y con el objeto desde consola ejecutar:
+./<script.o> &> <tuvieja.txt>
+
+*/
 void densAristasMitad(){		// hay una clique fija de 10 (excepto hasta menores a 20 que es de 4)
-	for(int i = 5; i < 601; i++){
+	int c; //Tamaño de la clique 
+	int n; //Tamaño del grafo
+	int d; //Tamaño de la densidad
+
+	for(int i = 5; i < 601; i++){ //601
 		for(int j = 0; j < 40; j++){
 			if(i <= 20){
-				cout << 4;
-				cout << i;
-				cout << 0.5;
+				c = 4;
+				n = i;
+				d = 0.5;
+				genGrafoEmi(c, n, d);
+
 			}else{
-				cout << 10;
-				cout << i;
-				cout << 0.5;
+				c = 10;
+				n = i;
+				d = 0.5;
+				genGrafoEmi(c, n, d);
 			}
 		}
-	}	
+	}
+	cout << -1 << endl;	//Para saber donde terminar de levantar el txt
 }
 
 void densGrafComp(){		// hay una clique fija de 10 (excepto hasta menores a 20 que es de 4)
-	for(int i = 5; i < 601; i++){
+	int c; //Tamaño de la clique 
+	int n; //Tamaño del grafo
+	int d; //Tamaño de la densidad
+
+	for(int i = 5; i < 50; i++){ //era 600
 		for(int j = 0; j < 40; j++){
-			cout << 4;
-			cout << i;
-			cout << 1;
+			c = 4;
+			n = i;
+			d = 1;
+			genGrafoEmi(c, n, d);
 		}
-	}	
+	}
+
+	cout << -1 << endl;	//Para saber donde terminar de levantar el txt
 }
 
 void densPocasArist(){		// hay una clique fija de 10 (excepto hasta menores a 20 que es de 4)
+	int c; //Tamaño de la clique 
+	int n; //Tamaño del grafo
+	int d; //Tamaño de la densidad
+
 	for(int i = 5; i < 601; i++){
 		for(int j = 0; j < 40; j++){
 			if(i <= 20){
-				cout << 4;
-				cout << i;
-				cout << 0.1;
+				c = 4;
+				n = i;
+				d = 0.1;
+				genGrafoEmi(c, n, d);
 			}else{
-				cout << 10;
-				cout << i;
-				cout << 0.1;
+				c = 10;
+				n = i;
+				d = 0.1;
+				genGrafoEmi(c, n, d);
 			}
 		}
-	}	
+	}
+
+	cout << -1 << endl;	//Para saber donde terminar de levantar el txt
 }
 
 int main(){
-	// densAristasMitad();
+	//densAristasMitad();
 	// densGrafComp();
-	// densPocasArist();
+	//densPocasArist();
 
 	return 0;
 }

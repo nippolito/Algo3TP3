b83c0364034de63a5733bcb4b877970f360d8df4
Gregorio <gregorio.freidin@gmail.com>
2017-06-23 00:01:10 -0300

    .

--- Generador de Grafos/script.cpp
+++ Generador de Grafos/script.cpp
@@ -1,45 +1,47 @@
 #include <iostream>
 
+#define CANT_NODOS 300
+
 
 using namespace std;
 
 void densAristasMitad(){		// hay una clique fija de 10 (excepto hasta menores a 20 que es de 4)
-	for(int i = 5; i < 601; i++){
+	for(int i = 5; i < CANT_NODOS; i++){
 		for(int j = 0; j < 40; j++){
 			if(i <= 20){
-				cout << 4;
-				cout << i;
-				cout << 0.5;
+				cout << 4<<" ";
+				cout << i<<" ";
+				cout << 0.5<< endl;
 			}else{
-				cout << 10;
-				cout << i;
-				cout << 0.5;
+				cout <<10<<" ";
+				cout << i<<" ";
+				cout << 0.5<< endl;
 			}
 		}
 	}	
 }
 
 void densGrafComp(){		// hay una clique fija de 10 (excepto hasta menores a 20 que es de 4)
-	for(int i = 5; i < 601; i++){
+	for(int i = 5; i < CANT_NODOS; i++){
 		for(int j = 0; j < 40; j++){
-			cout << 4;
-			cout << i;
-			cout << 1;
+			cout << 4<<" ";
+			cout << i<<" ";
+			cout << 1<< endl;
 		}
 	}	
 }
 
 void densPocasArist(){		// hay una clique fija de 10 (excepto hasta menores a 20 que es de 4)
-	for(int i = 5; i < 601; i++){
+	for(int i = 5; i < CANT_NODOS; i++){
 		for(int j = 0; j < 40; j++){
 			if(i <= 20){
-				cout << 4;
-				cout << i;
-				cout << 0.1;
+				cout << 4<<" ";
+				cout << i<<" ";
+				cout << 0.1<< endl;
 			}else{
-				cout << 10;
-				cout << i;
-				cout << 0.1;
+				cout << 10<<" ";
+				cout << i<<" ";
+				cout << 0.1 << endl;
 			}
 		}
 	}	
